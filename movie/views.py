2439e2347c0dd0f2582d57aba74601cc9faa8c6a
Esteban Salazar <estebansalazr2005@gmail.com>
2025-08-01 04:44:28 -0500

    busqueda de peliculas

--- movie/views.py
+++ movie/views.py
@@ -1,9 +1,16 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Movie
 
 # Create your views here.
 def home(request):
-    return render(request, 'home.html', {'name': 'Esteban Salazar Orozco'})
+    searchTerm = request.GET.get('searchMovie')
+    if searchTerm:
+        movies = Movie.objects.filter(tittle__icontains=searchTerm)
+    else:
+        movies = Movie.objects.all()
+
+    return render(request, 'home.html', {'searchTerm':searchTerm, 'movies':movies ,'name': 'Esteban Salazar Orozco'})
 
 def about(request):
     return render(request, 'about.html')
